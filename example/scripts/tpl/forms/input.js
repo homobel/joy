7ad48d5b12337e5e2a0542aeaa08a4473ae796de
homobel <homobel@gmail.com>
2018-05-12 23:27:37 +0300

    typos

--- example/scripts/tpl/forms/input.js
+++ example/scripts/tpl/forms/input.js
@@ -1,1 +1,3 @@
-define(['joy-runtime'], function(joy) {return function(data) {data=joy.o(data);return '<input type="text" value="'+joy.e(joy.p(data))+'" />\r\n';}});
+define(['joy-runtime'], function(joy) {
+return function(data) {data=joy.o(data);return '<input type="text" value="'+joy.e(joy.p(data))+'" />\r\n';}
+});
